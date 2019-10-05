c47108a955a7a7f6ea3230000332c24e58b8c4fc
lvivgeorge <lvivgeorge@gmail.com>
2019-10-05 23:29:06 +0300

    add screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,11 @@ export class AppComponent implements AfterViewInit {
 
   constructor(private playerService: PlayerService) {}
 
+  public saveScreen(evt: Event) {
+    const element = evt.target as HTMLAnchorElement;
+    element.href = this.playerService.saveScreen();
+  }
+
   public ngAfterViewInit() {
     this.setPlayer();
   }
--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 
-declare const jsmpeg: any;
+declare const jsmpeg;
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +11,14 @@ export class PlayerService {
   static SERVER_PORT = 3001;
 
   private canvas: HTMLCanvasElement;
+  private player: any;
 
   get fullServerAddress(): string {
     return `ws://${PlayerService.SERVER_ADDRESS}:${PlayerService.SERVER_PORT}/`;
   }
 
 
-  init(canvas: HTMLCanvasElement) {
+  init(canvas: HTMLCanvasElement): void {
     // Show loading notice
     this.canvas = canvas;
     const ctx = canvas.getContext('2d');
@@ -25,6 +26,11 @@ export class PlayerService {
 
     // Setup the WebSocket connection and start the player
     const client = new WebSocket(this.fullServerAddress);
-    const player = new jsmpeg(client, {canvas});
+    this.player = new jsmpeg(client, {canvas});
+  }
+
+  saveScreen(): string {
+    return this.canvas.toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
   }
 }
